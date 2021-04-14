6f45a0264c503136ce9f5411855d09b29ad236af
RomanDeveloperAcc <roma04dan@gmail.com>
2021-04-14 11:55:29 +0300

    Added drop all command attributes interface

---
+++ src/models/commands/drop-all-command-attributes.model.ts
@@ -0,0 +1,14 @@
+export interface DropAllCommandAttributes {
+	/**
+	 * Specifies the JDBC database connection URL
+	 */
+	url: string;
+	/**
+	 * Specifies the database username
+	 */
+	username: string;
+	/**
+	 * Specifies the database password
+	 */
+	password: string;
+}
