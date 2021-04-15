3ef35331395ddd9f8de13b8e975f3b96623c6e4e
Taylor Buckner <taylora.buckner@gmail.com>
2021-04-15 15:22:41 -0500

    (release): updated bundled Liquibase to match release v4.3.3

---
+++ dist/util/file-helper.d.ts
@@ -0,0 +1,5 @@
+export declare class FileHelper {
+    static get bundledLiquibasePath(): string;
+    static get bundledLiquibasePathForExternalConsumers(): string;
+    static get bundledLiquibasePathForInternalConsumers(): string;
+}
